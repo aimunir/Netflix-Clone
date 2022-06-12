5b6a2ed02ca464ee6080ccc881d581c550a87934
aimunir <aimunir25@outlook.com>
2022-06-12 15:08:37 +0600

    first commit

---
+++ src/HomeScreen/HomeScreen.js
@@ -0,0 +1,23 @@
+ import React from 'react'
+import Banner from '../Banner/Banner'
+ import Navbar from '../Navbar/Navbar'
+ import './HomeScreen.css'
+ 
+ const HomeScreen = () => {
+   return (
+     <div>
+             {/* navbar */}
+                  <Navbar/>
+                                
+                    
+        {/* banner */}
+                    <Banner/>
+        {/* row */}
+
+
+
+     </div>
+   )
+ }
+ 
+ export default HomeScreen
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='nav nav__black'>
+        <div className="nav-container">
+            <img className='nav_logo' src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+            
+            <img  className='nav_avatar' src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/bf6e4a33850498.56ba69ac3064f.png" alt="" />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
