4c1160b357a91bae61ed21afc12acf592db483bc
aimunir <aimunir25@outlook.com>
2022-06-18 10:40:34 +0600

    Router added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import axios from "axios";
 import HomeScreen from "./HomeScreen/HomeScreen";
 import "./App.css";
-import { Routes,Route} from "react-router-dom";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 
 
 function App() {
   return (
     <div className="app">    
+        <BrowserRouter>
         <Routes>
-            <Route path="./HomeScreen" element={ <HomeScreen />} />
+
+           <Route path="/" element={ <HomeScreen />} />
         </Routes>
+
+        </BrowserRouter>
           
     </div>
   );
